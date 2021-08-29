1aae72be709a606dc3c22c2c8a2d556c1a5f1e2b
kongruksiamza <kongruksiam123@gmail.com>
2021-08-30 03:47:16 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const name ="Bangkok"
+  const apiKey="54b4eb78009eff9ad25aa26c716f17f8"
+  const [city,setCity] = useState({})
+
+  useEffect(()=>{
+    const url=`http://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${apiKey}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>{
+        setCity(data)
+    })
+  },[])
+
+  const convertTemp=(k)=>{
+      return (k-273).toFixed()
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <section>
+            <div className="location">
+                <h1 className="city">{city.name}</h1>
+                <p className="state">{city.sys.country}</p>
+            </div>
+            <div className="card">
+                <div className="weather">
+                    <h1>{convertTemp(city.main.temp)}&deg;C</h1>
+                    <small>สูงสุด : {convertTemp(city.main.temp_max)}&deg;C , ต่ำสุด : {convertTemp(city.main.temp_min)}&deg;C</small>
+                </div>
+                <div className="info">
+                    <div className="status">{city.weather[0].main}</div>
+                    <div className="humidity">ความชื้น = {city.main.humidity}</div>
+                    <div className="wind">ความเร็วลม = {city.wind.speed}</div>
+                </div>
+            </div>
+        </section>
     </div>
   );
 }
